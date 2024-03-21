83630fd98e7be891ce1bfc4fccf90591e514fad9
danabagyam <dhanabagyamr@gmail.com>
2024-03-21 18:59:34 +0530

    task 2

--- basics/demo.py
+++ basics/demo.py
@@ -1,4 +1,30 @@
-numbers = [10,20,30,40,50,100,120,790]
-total=numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]
-Avg=total/8
-print(Avg)
+country = {
+    "id" : 1,
+    "country_name" : "India",
+    "code" : "+91",
+    "State" : ["Kerala", "Tamil nadu", "karnataka", "Andhra Pradesh", "Delhi"],
+     "City" : [{
+         "id": 1,
+         "name" : "chennai"
+     },
+     {
+         "id": 2,
+         "name": "Trivandrum"
+     },
+     {
+         "id": 3,
+         "name" : "Bangalore"
+     },{
+         "id": 4,
+         "name" : "Chittod"
+     },{
+         "id": 5,
+         "name" : "Delhi"
+     }]
+     
+}
+
+Name = "Danabagyam"
+Number = "666666666"
+
+print("My name is",Name,"and I'm from",country["City"][0]["name"],",",country["State"][1],",",country["country_name"],",", "and my mobile number is +91", Number)
