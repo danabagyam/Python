0f0ddf4a95478ef5e97e55a065445e8b528920bd
danabagyam <dhanabagyamr@gmail.com>
2024-03-21 18:12:52 +0530

    avg sum

--- basics/demo.py
+++ basics/demo.py
@@ -1,3 +1,4 @@
-number = [10, 20, 30, 10]
-
-print(number.find(10))
+numbers = [10,20,30,40,50,100,120,790]
+total=numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]
+Avg=total/8
+print(Avg)
